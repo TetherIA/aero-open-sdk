c65736f9dcba6f5c3a515c2ff531402c89206225
Mohit <mohityadav@tetheria.ai>
2025-09-30 14:09:02 -0700

    ADD: examples added

---
+++ examples/perform_homing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import time
+
+from aero_hand_lite.aero_hand import AeroHand
+
+
+if __name__ == "__main__":
+    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00")
+
+    ## Perfrom homing
+    ## NOTE: While performing homing, robot will not respond to any other commands.
+    ## Make sure the hand is in a safe position to perform homing.
+    hand.send_homing()
