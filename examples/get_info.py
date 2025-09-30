3dbc07cc89c3691b27d5e22a24f521d629d45573
Mohit <mohityadav@tetheria.ai>
2025-09-30 14:09:02 -0700

    ADD: examples added

---
+++ examples/get_info.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import time
+
+from aero_hand_lite.aero_hand import AeroHand
+
+
+if __name__ == "__main__":
+    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00")
+
+    while True:
+        motor_current = hand.get_motor_currents()
+        print("Motor Currents:", motor_current)
+        motor_positions = hand.get_motor_positions()
+        print("Motor Positions:", motor_positions)
+        motor_speed = hand.get_motor_speed()
+        print("Motor Speeds:", motor_speed)
+        motor_temperatures = hand.get_motor_temperatures()
+        print("Motor Temperatures:", motor_temperatures)
+        time.sleep(0.1)
--- examples/joint_control.py
+++ examples/joint_control.py
@@ -1,17 +1,25 @@
+#!/usr/bin/env python3
+import time
+
 from aero_hand_lite.aero_hand import AeroHand
 
 
 if __name__ == "__main__":
-    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:C8:1C-if00")
+    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00")
 
-    # hand.set_joint_positions([10.0, 20.0, 30.0, 40.0] + [45.0] * 12)
+    i = 0
+    dir = 1
     while True:
-        try:
-            hand.get_motor_currents()
-        except Exception as e:
-            print("Error:", e)
-            continue
-    # temps = hand.get_motor_currents()
-    # hand.close()
-
-    # print("Curent:", temps)
+        ## Stagnant thumb + Move Fingers
+        finger_joint_ranges = [ul - ll for ul, ll in zip(hand.joint_upper_limits, hand.joint_lower_limits)]
+        joint_pos = [0.0] * 4 + [finger_joint_ranges[4 + j] * i / 100.0 for j in range(12)]
+        hand.set_joint_positions(joint_pos)
+        if dir == 1:
+            i += 1
+            if i >= 100:
+                dir = -1
+        else:
+            i -= 1
+            if i <= 0:
+                dir = 1
+        time.sleep(0.01)
