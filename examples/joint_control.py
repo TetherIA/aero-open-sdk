c94b8dba6995161343ba71d801b0ce12d7a140b8
Mohit <mohityadav@tetheria.ai>
2025-10-01 12:30:26 -0700

    UPDATE: examples files updated

--- examples/joint_control.py
+++ examples/joint_control.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import time
-
 from aero_hand_lite.aero_hand import AeroHand
 
-
 if __name__ == "__main__":
-    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00")
+    hand = AeroHand(
+        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00"
+    )
 
     i = 0
     dir = 1
     while True:
         ## Stagnant thumb + Move Fingers
-        finger_joint_ranges = [ul - ll for ul, ll in zip(hand.joint_upper_limits, hand.joint_lower_limits)]
-        joint_pos = [0.0] * 4 + [finger_joint_ranges[4 + j] * i / 100.0 for j in range(12)]
+        finger_joint_ranges = [
+            ul - ll for ul, ll in zip(hand.joint_upper_limits, hand.joint_lower_limits)
+        ]
+        joint_pos = [0.0] * 4 + [
+            finger_joint_ranges[4 + j] * i / 100.0 for j in range(12)
+        ]
         hand.set_joint_positions(joint_pos)
         if dir == 1:
             i += 1
--- examples/perform_homing.py
+++ examples/perform_homing.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-import time
-
 from aero_hand_lite.aero_hand import AeroHand
 
-
 if __name__ == "__main__":
-    hand = AeroHand("/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00")
+    hand = AeroHand(
+        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00"
+    )
 
     ## Perfrom homing
     ## NOTE: While performing homing, robot will not respond to any other commands.
