e9f52af43c1f6b59ba56927f5389cb9fc5e51d00
Harsh-Panara <harshpanara@tetheria.ai>
2025-10-02 11:15:19 -0700

    homing done

--- examples/perform_homing.py
+++ examples/perform_homing.py
@@ -3,9 +3,9 @@ from aero_hand_lite.aero_hand import AeroHand
 
 if __name__ == "__main__":
     hand = AeroHand(
-        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00"
+        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00" 
     )
-
+    
     hand.send_homing()
     try:
         while True:
