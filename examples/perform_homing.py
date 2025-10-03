760a16cdf467a3f23da8da2e862a945012a976d8
Harsh-Panara <harshpanara@tetheria.ai>
2025-10-03 13:43:02 -0700

    added app folder , and changes to homing added comments

--- examples/perform_homing.py
+++ examples/perform_homing.py
@@ -5,5 +5,8 @@ if __name__ == "__main__":
     hand = AeroHand(
         "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00" 
     )
-    
+
+    ## Perform homing
+    ## NOTE: While performing homing, robot will not respond to any other commands unless it receives an ACK under a given timeout.
+    ## Make sure the hand is in a safe position to perform homing.
     hand.send_homing()
