475cebc75446b8ba8b6e76f1440584d0bda303dd
Mohit <mohityadav@tetheria.ai>
2025-10-08 10:45:24 -0700

    PORT

--- examples/perform_homing.py
+++ examples/perform_homing.py
@@ -3,7 +3,7 @@ from aero_hand_open.aero_hand import AeroHand
 
 if __name__ == "__main__":
     hand = AeroHand(
-        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:CA:08-if00" 
+        "/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_D8:3B:DA:45:C8:1C-if00" 
     )
 
     ## Perform homing
